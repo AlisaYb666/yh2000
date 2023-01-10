58436bd39e4c9697cee615c089bb9cf245e21ef6
阿衰 <98404971+PHSY666@users.noreply.github.com>
2023-01-11 00:08:44 +0800

    Update 区域截屏.js

--- AutojsPro常用函数/区域截屏.js
+++ AutojsPro常用函数/区域截屏.js
@@ -5,7 +5,7 @@ if (!requestScreenCapture()) {
 var img = captureScreen();
  //在坐标90,220正方形的左上角为中心点长款为400*400像素的区域
 var clip = images.clip(img, 90, 220, 400, 400);
- 
+ //保存到相册路径
 images.save(clip, "/storage/emulated/0/临时库/1.jpg");
 
 //回收clip
